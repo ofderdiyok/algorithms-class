db5c75ad48e21ed9a92565af10a3d22fc081529f
Faruk Derdiyok <farukderdiyok01@gmail.com>
2023-02-21 14:41:24 +0300

    Create InsertionSort2.java

---
+++ Hackerrank/InsertionSort2.java
@@ -0,0 +1,22 @@
+class Result {
+
+    public static void insertionSort2(int n, List<Integer> arr) {
+        
+        for(int i=1; i<n; i++){
+            int key = arr.get(i);
+            int j = i-1;
+            
+            while (j >= 0 && arr.get(j) > key) {
+                arr.set(j+1, arr.get(j));
+                j = j - 1;
+            }
+            arr.set(j+1, key);
+            for(int k=0; k<arr.size(); k++){
+                System.out.print(arr.get(k) + " ");
+            }
+            System.out.println("");
+        }
+        
+    }
+
+}
