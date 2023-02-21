90c00135b31fb6b457dc6ac9bec67d32b54e9c94
Faruk Derdiyok <farukderdiyok01@gmail.com>
2023-02-21 14:40:26 +0300

    Add files via upload

---
+++ Hackerrank/InsertionSort1.java
@@ -0,0 +1,34 @@
+// Hackerrank Algorithm - Insertion Sort 1 Solution
+
+import java.util.*;
+
+class Result {
+
+    public static void insertionSort1(int n, List<Integer> arr) {
+        // Write your code here
+
+        int temp;
+
+        for(int i=n-1; i>0; i--){
+
+            temp = arr.get(i);
+
+            if(arr.get(i)<arr.get(i-1)){
+                arr.set(i, arr.get(i-1));
+
+                for(int j=0; j<n; j++){
+                    System.out.print(arr.get(j) + " ");
+                }
+                System.out.println("");
+
+                arr.set(i-1, temp);
+            }
+        }
+
+        for(int j=0; j<arr.size(); j++){
+            System.out.print(arr.get(j) + " ");
+        }
+
+    }
+
+}
